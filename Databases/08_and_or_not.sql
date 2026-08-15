1eb29c091a626fd2d4c405a1285128e2ef4b30ba
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-15 16:25:32 -0500

    Agregar archivo 08_and_or_not.sql con sintaxis y ejemplos para mejorar la comprensión del uso de las cláusulas AND, OR y NOT en SQL

---
+++ Databases/08_and_or_not.sql
@@ -0,0 +1,27 @@
+-- Active: 1786813471273@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis de and
+
+SELECT columnas FROM [nombre_de_la_tabla] WHERE condicion1 AND condicion2
+
+-- NOTA de and: Este comando solo muestra los datos si se cumplen todas las reglas al mismo tiempo
+
+-- Sintaxis de or
+
+SELECT columnas FROM tabla WHERE condicion1 OR condicion
+
+-- NOTA de or: Este comando devuelve un registro si al menos una de las condiciones es verdadera
+
+-- Sintaxis de not
+
+SELECT columnas FROM [nombre_de_la_tabla] WHERE NOT condicion
+
+-- NOTA de not: Este comando devuelve solo los registros que no cumplen la condicion
+
+-- Ejemplo:
+
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' -- Este comando devuelve solo a los usuarios que no cumplen la condicion
+
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' AND age = 15 -- Este comando devuelve solo a los usuarios que cumplen la condicion
+
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15 -- Este comando devuelve solo a los usuarios que cumplan una de las dos condicion
