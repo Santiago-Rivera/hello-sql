d4c5953da5eea9e65aa9014a4bd19e7a0b557109
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-15 16:36:01 -0500

    Agregar archivo 09_limit.sql con sintaxis y ejemplos para mejorar la comprensión del uso de la cláusula LIMIT en SQL

---
+++ Databases/09_limit.sql
@@ -0,0 +1,13 @@
+-- Active: 1786813471273@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+SELECT  columna1, columna2 FROM [nombre_de_la_tabla] LIMIT [numero_de_filas]
+
+-- NOTA: Este comando sirve para restringir la cantidad de filas que devuelve una consulta SELECT
+
+-- Ejemplo:
+
+SELECT * FROM users LIMIT 3 -- Este comando sirve para traer los primeros tres datos de una tabla
+
+SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15 LIMIT 2 -- Este comando sirve para solo traer a los usuarios que cumplen una de las dos condiciones
