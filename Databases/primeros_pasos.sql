9183426e359cf4afa6f0774e0ef2b56003f172d4
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-15 12:25:52 -0500

    Agregar ejemplos y comentarios en el archivo primeros_pasos.sql para mejorar la comprensión de la creación de bases de datos

--- Databases/primeros_pasos.sql
+++ Databases/primeros_pasos.sql
@@ -1,2 +1,9 @@
 -- Active: 1786770905739@@127.0.0.1@3306@hello_mysql
-CREATE DATABASE hello_mysql
+
+-- Sintaxis para crear una base de datos
+
+CREATE DATABASE [nombre_de_la_base_de_datos] -- Este comando sirve para crear una base de datos
+
+-- Ejemplo:
+
+CREATE DATABASE hello_mysql -- Así se debe usar el comando para crear la base de datos
