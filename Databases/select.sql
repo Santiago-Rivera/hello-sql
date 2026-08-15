ac4c38ce6b7c245e8ae83be2202da509553d1a64
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-15 13:23:41 -0500

    Agregar archivo select.sql con sintaxis y ejemplos para mejorar la comprensión de las consultas en SQL

---
+++ Databases/select.sql
@@ -0,0 +1,14 @@
+-- Active: 1786813471273@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+SELECT * FROM [nombre_de_la_tabla] -- Este comando sirve para consultar todos los datos de una tabla
+
+-- Ejemplo:
+
+SELECT * FROM users -- Trae todos los datos de hay en la tabla
+
+SELECT name FROM users -- Trae todos los datos de una columna especifica de la tabla
+
+SELECT user_id, name FROM users -- Trae todos los datos de dos o varias columnas especificas de la tabla
+
