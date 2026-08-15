9fb530a3ad5fd3f41e1600e46726fe22b3b2cbf8
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-15 14:02:54 -0500

    Agregar ejemplos adicionales en 05_where.sql para mejorar la comprensión del uso de la cláusula WHERE y el comando DISTINCT en SQL

--- Databases/05_where.sql
+++ Databases/05_where.sql
@@ -10,4 +10,12 @@ SELECT columna1, columna2 FROM [nombre_de_la_tabla] WHERE condicion
 
 SELECT * FROM users WHERE age = 15 -- Este comando solo trae a usuarios que tengan 15 años
 
-SELECT * FROM users WHERE surname = "Asaustre" -- Este comando solo trae al usuario que tenga ese apellido
+SELECT * FROM users WHERE surname = "Asaustre" -- Este comando solo trae al usuario que tenga ese
+
+SELECT name FROM users WHERE age = 15 -- Este comando solo trae los nombres que cumplan con la misma condicion
+
+-- Usando el comando DISTINCT
+
+SELECT DISTINCT name FROM users WHERE age = 15 -- Este comando trae los nombres que son distintos pero que cumplen con la condicion de la edad
+
+SELECT DISTINCT age FROM users WHERE age = 15 -- Este comando solo trae la edad de dos usuarios pero no la repite
