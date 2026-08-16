78c893a8d0e13d6ac73573900567c8c2ec90387b
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-16 11:39:07 -0500

    Agregar archivo 15_avg.sql con sintaxis y ejemplos para mejorar la comprensión del uso de la función AVG en SQL

---
+++ Databases/15_avg.sql
@@ -0,0 +1,11 @@
+-- Active: 1786898004073@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+SELECT AVG(nombre_columna) FROM [nombre_de_la_tabla]
+
+-- NOTA: Este comando calcula la media aritmetica de los valores numericos en una columna especifica e ignora automaticamente los valores nulos
+
+-- Ejemplo:
+
+SELECT AVG(age) FROM users
