ff3a488e1443ca4a955472ad0ad1ec275c5c6b69
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-17 14:06:34 -0500

    Agregar archivo 03_relationships.sql con sintaxis y ejemplos para las relaciones de una tabla en SQL

---
+++ Databases/04_Tables/04_relationships.sql
@@ -0,0 +1,97 @@
+-- Active: 1786898004073@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+
+CREATE TABLE pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE,
+    cliente_id INT,
+    CONSTRAINT fk_cliente FOREIGN KEY (cliente_id)
+    REFERENCES clientes(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- NOTA: Para relacionar tablas se usan las claves foraneas (FOREIGN KEY) que se conectan con las claves primarias (PRIMARY KEY) de otra tabla
+
+-- Ejemplo:
+
+-- Relaciones 1:1 (uno a uno):
+
+CREATE TABLE dni(
+    dni_id int AUTO_INCREMENT PRIMARY KEY,
+    dni_number int NOT NULL,
+    user_id int,
+    UNIQUE(dni_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+)
+
+-- Relaciones 1:N (uno a muchos):
+
+CREATE TABLE companies(
+    company_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL
+)
+
+-- Relaciones N:M (muchos a muchos):
+
+CREATE TABLE languages(
+    language_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL
+)
+
+CREATE TABLE users_languages(
+    users_language_id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int,
+    language_id int,
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(language_id) REFERENCES languages(language_id),
+    UNIQUE(user_id, language_id)
+)
+
+/*
+Relaciones de Auto-Referencia
+Relación que indica que un un registro en la tabla A puede
+relacionarse con otro registro de la tabla A.
+*/
+
+-- Usando INSERT INTO:
+
+-- "dni" (Relación 1:1)
+
+INSERT INTO dni (dni_number, user_id) VALUES (11111111, 1);
+INSERT INTO dni (dni_number, user_id) VALUES (22222222, 2);
+INSERT INTO dni (dni_number, user_id) VALUES (33333333, 3);
+INSERT INTO dni (dni_number) VALUES (44444444);
+
+-- "companies" y "users"  (Relación 1:N)
+
+INSERT INTO companies (name) VALUES ('MoureDev');
+INSERT INTO companies (name) VALUES ('Apple');
+INSERT INTO companies (name) VALUES ('Google');
+
+UPDATE users SET company_id = 1 WHERE user_id = 1;
+UPDATE users SET company_id = 2 WHERE user_id = 3;
+UPDATE users SET company_id = 3 WHERE user_id = 4;
+UPDATE users SET company_id = 1 WHERE user_id = 7;
+
+-- "languages" y "users_languages"  (Relación N:M)
+
+INSERT INTO languages (name) VALUES ('Swift');
+INSERT INTO languages (name) VALUES ('Kotlin');
+INSERT INTO languages (name) VALUES ('JavaScript');
+INSERT INTO languages (name) VALUES ('Java');
+INSERT INTO languages (name) VALUES ('Python');
+INSERT INTO languages (name) VALUES ('C#');
+INSERT INTO languages (name) VALUES ('COBOL');
+
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 1);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 2);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 5);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 3);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 5);
