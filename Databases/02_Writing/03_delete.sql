92338d758a2d61040eda56891effa5f95068f98c
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-16 22:29:53 -0500

    Agregar archivo 03_delete.sql con sintaxis y ejemplos para eliminar datos de una tabla específica

---
+++ Databases/02_Writing/03_delete.sql
@@ -0,0 +1,11 @@
+-- Active: 1786898004073@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+DELETE FROM [nombre_de_la_tabla] WHERE condición
+
+-- NOTA: Este comando se usa para eliminar datos ya guardados
+
+-- Ejemplo:
+
+DELETE FROM users WHERE user_id = 11
