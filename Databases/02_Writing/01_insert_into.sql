e56ea20f09051f873c1eaf20ab0f2b63f6bcf7d8
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-16 22:13:46 -0500

    Agregar archivo 01_insert_into.sql con sintaxis y ejemplos para insertar datos en una tabla específica

---
+++ Databases/02_Writing/01_insert_into.sql
@@ -0,0 +1,15 @@
+-- Active: 1786898004073@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+INSERT INTO tabla (columna1, columna2, columna3, columna4, columna5, columna6) VALUES (valor1, valor2, valor3, valor4, valor5, valor6)
+
+-- NOTA: Este comando sirve para insertar datos en una tabla específica
+
+-- Ejemplo:
+
+INSERT INTO users (user_id, name, surname) VALUES ("8", "Maria", "Lopez")
+
+INSERT INTO users (name, surname) VALUES ("Paco", "Perez")
+
+INSERT INTO users (user_id, name, surname) VALUES ("11", "EL", "Merma")
