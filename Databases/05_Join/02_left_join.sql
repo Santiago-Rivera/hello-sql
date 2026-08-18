7e6663d6d8b4fa5b8e51bd1b505d8e2728e25a3b
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-08-18 17:35:58 -0500

    Agregar archivo 02_left_join.sql con sintaxis y ejemplos para realizar uniones izquierdas en SQL

---
+++ Databases/05_Join/02_left_join.sql
@@ -0,0 +1,29 @@
+-- Active: 1786898004073@@127.0.0.1@3306@hello_mysql
+
+-- Sintaxis
+
+SELECT columnas
+FROM tabla1 AS t1
+LEFT JOIN tabla2 AS t2
+ON t1.columna_comun = t2.columna_comun;
+
+-- NOTA: Esta instrucción selecciona todos los registros de la tabla izquierda y las filas coincidentes de la tabla derecha
+
+-- Ejemplo:
+
+SELECT * FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+
+SELECT name, dni_number FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+
+SELECT name, dni_number FROM dni
+LEFT JOIN users
+ON users.user_id = dni.user_id
+
+SELECT users.name, languages.name
+FROM users
+LEFT JOIN users_languages ON users.user_id=users_languages.user_id
+LEFT JOIN languages ON users_languages.language_id=languages.language_id
